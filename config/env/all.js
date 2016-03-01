6574304a67621a7fe92a4a28ed18ebd715b4a151
keminggu <342802259@qq.com>
2016-03-01 23:55:51 +0800

    修改jwt-secret

--- config/env/all.js
+++ config/env/all.js
@@ -8,6 +8,6 @@ module.exports = {
     },
 	mongo_db: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ido',
 	redis_db: process.env.REDIS_URI || 'redis://127.0.0.1:6379',
-	jwt_secret: process.env.JWT_SECRET || 'test',
+	jwt_secret: process.env.JWT_SECRET || 'jwt_secret',
 	token_exp: 60 * 60,
 };
